35cf4cdad434ec807e7c3a8509f61a0d1f4110cf
LarsKalishoek <99069312@mydavinci.nl>
2023-04-05 13:35:42 +0200

    get opdracht gemaakt

---
+++ welcome.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+
+    <h1>De ingevulde gegevens zijn:</h1><br>
+    Naam: <?php echo $_GET["name"]; ?><br>
+    Email: <?php echo $_GET["email"]; ?><br>
+
+</body>
+</html>
