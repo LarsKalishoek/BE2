ae08eebe0541aef1ed49f91dcd1131f036a8e588
LarsKalishoek <99069312@mydavinci.nl>
2023-04-06 14:31:36 +0200

    post gebruikt

--- welcome.php
+++ welcome.php
@@ -3,8 +3,8 @@
 <body>
 
     <h1>De ingevulde gegevens zijn:</h1><br>
-    Naam: <?php echo $_GET["name"]; ?><br>
-    Email: <?php echo $_GET["email"]; ?><br>
+    Naam: <?php echo $_POST['fname']; ?><br>
+    Email: <?php echo $_POST['email']; ?><br>
 
 </body>
 </html>
