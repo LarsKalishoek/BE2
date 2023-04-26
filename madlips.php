de23bae78f51c1bbbedeb5476ca7d2e179ae04fd
LarsKalishoek1 <lars.kalishoek@student.divd.nl>
2023-04-26 19:34:50 +0200

    add files

---
+++ madlips.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="page">
+        <div><h1>Mad Libs</h1></div>
+        <div class="topnav">
+            <a class="active" href="#onkunde">Onkunde</a>
+            <a href="#paniek">Er heerst paniek</a>
+        </div>
+        <div class="form">
+            <form action="" method="post">
+                <div style="display: flex;">
+                    <div class = "text" style="width: 50%; text-align: center">
+                        <div>Welk dier zou je nooit als huisdier willen hebben?</div>
+                        <div>Wie is de belangrijkste persoon in je leven?</div>
+                        <div>In welk land zou je graag willen wonen?</div>
+                        <div>Wat doe je als je je verveelt?</div>
+                        <div>Met welk speelgoed speelde je als kind het meest?</div>
+                        <div>Bij welke docent spijbel je het liefst?</div>
+                        <div>Als je €100.000,- had, wat zou je dan kopen?</div>
+                        <div>Wat is je favoriete bezigheid?</div>
+                    </div>
+                    <div class="text" style="width: 50%; text-align: center;" >
+                        <div><input type="text" name="" id=""></div>
+                        <div><input type="text" name="" id=""></div>
+                        <div><input type="text" name="" id=""></div>
+                        <div><input type="text" name="" id=""></div>
+                        <div><input type="text" name="" id=""></div>
+                        <div><input type="text" name="" id=""></div>
+                        <div><input type="text" name="" id=""></div>
+                        <div><input type="text" name="" id=""></div>
+                        <input type="submit" name="SubmitButton">
+                    <!-- </div>
+                    <div class = "button">
+                    </div> -->
+                </div>
+            </form>
+        </div>
+</div>
+    </body>
+</html>
